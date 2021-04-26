9779706c4fe9f23d0dc13fc5195acd440b0265c4
unknown <nicolapirruccio@icloud.com>
2021-04-25 21:24:48 -0700

    more heroku changes

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import socketio from 'socket.io-client';
-const SOCKET_URL = 'http://localhost:3001';
+const socketURL =
+  process.env.NODE_ENV === 'production'
+    ? window.location.hostname
+    : 'https://localhost:3001';
 
-export const socket = socketio.connect(SOCKET_URL);
+export const socket = socketio.connect(socketURL, { secure: true });
 export const SocketContext = React.createContext();
