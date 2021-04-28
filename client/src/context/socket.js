d83daa3f53501218065559862767d775e4bfb9c9
unknown <nicolapirruccio@icloud.com>
2021-04-27 19:42:53 -0700

    final product

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -4,7 +4,7 @@ import socketio from 'socket.io-client';
 const socketURL =
   process.env.NODE_ENV === 'production'
     ? 'https://sockets-testapp.herokuapp.com'
-    : 'http://localhost:3001';
+    : 'http://4a30998bd985.eu.ngrok.io';
 
 export const socket = socketio.connect(socketURL);
 export const SocketContext = React.createContext();
